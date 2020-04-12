d7725514c1e001787b0333b91d775f996e890a9f
Gennady Petukhov <g3pmail@gmail.com>
2020-04-12 16:54:23 +0300

    Refactor Observable

--- 03-Hello/src/main/java/com/gpetuhov/rxjava/hello/Main.java
+++ 03-Hello/src/main/java/com/gpetuhov/rxjava/hello/Main.java
@@ -1,29 +1,17 @@
 package com.gpetuhov.rxjava.hello;
 
-import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.ObservableEmitter;
-import io.reactivex.rxjava3.core.ObservableOnSubscribe;
-import io.reactivex.rxjava3.functions.Consumer;
 
 public class Main {
 
     public static void main(String[] args) {
         // Subject in RxJava is called Observable
-        Observable<String> source = Observable.create(new ObservableOnSubscribe<String>() {
-            @Override
-            public void subscribe(@NonNull ObservableEmitter<String> emitter) throws Throwable {
-                emitter.onNext("Hello");
-                emitter.onNext("RxJava");
-            }
+        Observable<String> source = Observable.create(emitter -> {
+            emitter.onNext("Hello");
+            emitter.onNext("RxJava");
         });
 
         // This is the Observer
-        source.subscribe(new Consumer<String>() {
-            @Override
-            public void accept(String s) throws Throwable {
-                System.out.println(s);
-            }
-        });
+        source.subscribe(s -> System.out.println(s));
     }
 }
