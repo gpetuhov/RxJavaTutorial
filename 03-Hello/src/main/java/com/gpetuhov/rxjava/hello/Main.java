a85538f199380b632d894e00252c9e0a8126d338
Gennady Petukhov <g3pmail@gmail.com>
2020-04-12 16:53:17 +0300

    Add Observable

--- 03-Hello/src/main/java/com/gpetuhov/rxjava/hello/Main.java
+++ 03-Hello/src/main/java/com/gpetuhov/rxjava/hello/Main.java
@@ -1,8 +1,29 @@
 package com.gpetuhov.rxjava.hello;
 
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.functions.Consumer;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        // Subject in RxJava is called Observable
+        Observable<String> source = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> emitter) throws Throwable {
+                emitter.onNext("Hello");
+                emitter.onNext("RxJava");
+            }
+        });
+
+        // This is the Observer
+        source.subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Throwable {
+                System.out.println(s);
+            }
+        });
     }
 }
