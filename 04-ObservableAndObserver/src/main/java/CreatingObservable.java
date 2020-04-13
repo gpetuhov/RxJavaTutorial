b4316173f92f7308ec6ede1021d76b079d39ddba
Gennady Petukhov <g3pmail@gmail.com>
2020-04-13 19:39:27 +0300

    Add other ways of creating observables

--- 04-ObservableAndObserver/src/main/java/CreatingObservable.java
+++ 04-ObservableAndObserver/src/main/java/CreatingObservable.java
@@ -1,10 +1,15 @@
 import io.reactivex.rxjava3.core.Observable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Different ways of creating Observables
 public class CreatingObservable {
 
     public static void main(String[] args) {
+
         // === create() ===
+
         Observable<Integer> source = Observable.create(emitter -> {
             emitter.onNext(10);
             emitter.onNext(11);
@@ -13,5 +18,20 @@ public class CreatingObservable {
 
         // This will print only 10 and 11, because it has no consumer for onComplete
         source.subscribe(System.out::println);
+
+        // === just() ===
+
+        Observable<Integer> just = Observable.just(1, 2, 3);
+        just.subscribe(System.out::println);
+
+        // === fromIterable() ===
+
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Grape");
+        fruits.add("Peach");
+        Observable<String> fromIterable = Observable.fromIterable(fruits);
+
+        fromIterable.subscribe(System.out::println);
     }
 }
