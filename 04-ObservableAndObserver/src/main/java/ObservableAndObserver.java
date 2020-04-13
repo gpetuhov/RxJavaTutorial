b9b5e7180abed27c1215455598e8e0ebbbc59844
Gennady Petukhov <g3pmail@gmail.com>
2020-04-13 19:21:12 +0300

    Add Observable and Observer

---
+++ 04-ObservableAndObserver/src/main/java/ObservableAndObserver.java
@@ -0,0 +1,49 @@
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.internal.operators.observable.ObservableCreate;
+
+public class ObservableAndObserver {
+
+    public static void main(String[] args) {
+        Observable<Integer> source = new ObservableCreate<Integer>(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Throwable {
+                try {
+                    emitter.onNext(10);
+                    emitter.onNext(11);
+                    emitter.onComplete();
+                } catch (Exception e) {
+                    emitter.onError(e);
+                }
+            }
+        });
+
+        Observer<Integer> observer = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                System.out.println("Subscribed");
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                System.out.println("Received: " + integer);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                System.out.println("Error");
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Complete");
+            }
+        };
+
+        source.subscribe(observer);
+    }
+}
