96de72d35f54cf68c3819aa7e6b567c1be2be35b
Gennady Petukhov <g3pmail@gmail.com>
2020-04-17 19:02:28 +0300

    Add Maybe

--- 04-ObservableAndObserver/src/main/java/ObservableVariants.java
+++ 04-ObservableAndObserver/src/main/java/ObservableVariants.java
@@ -1,7 +1,5 @@
 import io.reactivex.rxjava3.annotations.NonNull;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Single;
-import io.reactivex.rxjava3.core.SingleObserver;
+import io.reactivex.rxjava3.core.*;
 import io.reactivex.rxjava3.disposables.Disposable;
 
 public class ObservableVariants {
@@ -9,6 +7,7 @@ public class ObservableVariants {
     public static void main(String[] args) {
 
         // === Single ===
+        // Emit only 1 item
 
         // This is ordinary observable
         Observable<String> source = Observable.just("Apple", "Grape", "Strawberry");
@@ -36,5 +35,34 @@ public class ObservableVariants {
         // Another way of creating Single
         Single.just("Plum")
                 .subscribe(System.out::println);
+
+        // === Maybe ===
+        // Emit 0 or 1 item
+
+        source.firstElement()
+                .subscribe(new MaybeObserver<String>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        System.out.println("Maybe subscibe");
+                    }
+
+                    // This will be called, if the element is emitted
+                    @Override
+                    public void onSuccess(@NonNull String s) {
+                        System.out.println(s);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        System.out.println("Maybe error");
+                    }
+
+                    // This will be called, if there are not elements,
+                    // for example if we subscribe to Observable.empty().firstElement();
+                    @Override
+                    public void onComplete() {
+                        System.out.println("Maybe complete");
+                    }
+                });
     }
 }
