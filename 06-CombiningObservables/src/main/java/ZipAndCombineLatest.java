4dac0871c05ab5105b69bc485e57d7c26a1ee941
Gennady Petukhov <g3pmail@gmail.com>
2020-04-18 18:03:44 +0300

    Add combineLatest

--- 06-CombiningObservables/src/main/java/ZipAndCombineLatest.java
+++ 06-CombiningObservables/src/main/java/ZipAndCombineLatest.java
@@ -12,7 +12,7 @@ public class ZipAndCombineLatest {
         // zip will wait for the second source to emit an item to create the resulting item.
         // In this example source1 emits 10 items in 10 seconds,
         // and source2 emits 10 items in 2 seconds.
-        // The resulting sequence will emit item every second,
+        // The resulting sequence will emit 10 items in 10 seconds,
         // and each item will be created from the corresponding pair of the source items.
         Observable.zip(source1, source2, (item1, item2) -> "Source 1: " + item1 + ", Source 2: " + item2)
                 .subscribe(System.out::println);
@@ -20,5 +20,15 @@ public class ZipAndCombineLatest {
         Thread.sleep(10000);
 
         System.out.println();
+
+        // CombineLatest emits an item every time it receives an item from any of the source
+        // by combining latest items received.
+        // If there are items from only one source,
+        // then combineLatest will wait for the first item from the second source before emitting.
+        // All previous items received from the first source will be ignored.
+        Observable.combineLatest(source1, source2, (item1, item2) -> "Source 1: " + item1 + ", Source 2: " + item2)
+                .subscribe(System.out::println);
+
+        Thread.sleep(10000);
     }
 }
