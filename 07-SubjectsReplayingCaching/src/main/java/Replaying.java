8dac3030b27b447911830a2fe3c89818c1ec4567
Gennady Petukhov <g3pmail@gmail.com>
2020-04-20 19:39:14 +0300

    Add replay

---
+++ 07-SubjectsReplayingCaching/src/main/java/Replaying.java
@@ -0,0 +1,28 @@
+import io.reactivex.rxjava3.core.Observable;
+import java.util.concurrent.TimeUnit;
+
+public class Replaying {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Observable<Long> source = Observable.interval(1, TimeUnit.SECONDS)
+                // Replay returns a ConnectableObservable (hot observable)
+                .replay()
+                // This is needed for our ConnectableObservable to start emitting
+                .autoConnect();
+
+        // Subscribe first observer
+        source.subscribe(number -> System.out.println("Observer 1: " + number));
+
+        // Wait for 5 seconds
+        Thread.sleep(5000);
+
+        // Subscribe second observer.
+        // Replay re-emits all items received from the moment autoConnect has been called.
+        // So, our second observer will receive items 0, 1, 2, 3, 4 right after it subscribes.
+        // And then it will continue receiving items 5, 6, 7, etc at the same time as the first observer.
+        source.subscribe(number -> System.out.println("Observer 2: " + number));
+
+        Thread.sleep(5000);
+    }
+}
