6def0465432875527a524321808dbbde5317690a
Gennady Petukhov <g3pmail@gmail.com>
2020-04-22 19:21:33 +0300

    Add AddEmissions sample

---
+++ 07-SubjectsReplayingCaching/src/main/java/AddEmissions.java
@@ -0,0 +1,28 @@
+import io.reactivex.rxjava3.subjects.PublishSubject;
+import io.reactivex.rxjava3.subjects.Subject;
+
+public class AddEmissions{
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Subject<String> subject = PublishSubject.create();
+
+        subject.subscribe(System.out::println);
+
+        // We can manually emit items to the Subject
+        subject.onNext("Hello");
+        subject.onNext("world");
+        
+        // After we have called onComplete, no further items will be emitted
+        subject.onComplete();
+        subject.onNext("Blablabla");
+
+        // Subjects are not thread-safe by default.
+        // To make them thread-safe we should call toSerialized
+        Subject<String> subject2 = PublishSubject.create();
+        Subject<String> serialized = subject2.toSerialized();
+
+        serialized.subscribe(System.out::println);
+        serialized.onNext("Hello 222");
+    }
+}
