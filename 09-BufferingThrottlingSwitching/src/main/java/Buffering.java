925d81d2e838e23b7e91f90e7f11b6802a3ea4c9
Gennady Petukhov <g3pmail@gmail.com>
2020-04-25 12:40:05 +0300

    Add time-based buffer

--- 09-BufferingThrottlingSwitching/src/main/java/Buffering.java
+++ 09-BufferingThrottlingSwitching/src/main/java/Buffering.java
@@ -1,5 +1,7 @@
 import io.reactivex.rxjava3.core.Observable;
 
+import java.util.concurrent.TimeUnit;
+
 public class Buffering {
 
     public static void main(String[] args) throws InterruptedException {
@@ -8,5 +10,15 @@ public class Buffering {
         Observable.range(1, 30)
                 .buffer(4)
                 .subscribe(System.out::println);
+
+        System.out.println();
+
+        // Here we will get lists of items every 2 seconds
+        Observable.interval(500, TimeUnit.MILLISECONDS)
+                // Time-based buffer operates on the computation scheduler
+                .buffer(2, TimeUnit.SECONDS)
+                .subscribe(System.out::println);
+
+        Thread.sleep(10000);
     }
 }
